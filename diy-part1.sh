6d896890e27e0db4f20a3cfc174f7d0b70c6a56a
jean0032 <55117470+jean0032@users.noreply.github.com>
2021-06-21 20:15:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,17 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-#git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git -b 18.06 clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos
 #git clone https://github.com/project-openwrt/luci-app-koolproxyR package/lean/luci-app-koolproxyR
 git clone https://github.com/destan19/OpenAppFilter package/lean/OpenAppFilter
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
+# git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+# git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
+# git clone https://github.com/aa65535/openwrt-chinadns.git package/chinadns
